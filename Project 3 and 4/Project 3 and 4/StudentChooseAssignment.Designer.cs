e69f905fc077cd2b6af0d01fcfe2fc7b83cdf8ba
Hoang Dinh <Hoang Dinh>
2017-11-27 15:21:52 -0600

    remove questions box

--- Project 3 and 4/Project 3 and 4/StudentChooseAssignment.Designer.cs
+++ Project 3 and 4/Project 3 and 4/StudentChooseAssignment.Designer.cs
@@ -31,9 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.nov17AssignmentsBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
-            this._Project3_4DatabaseDataSet = new Project_3_and_4._Project3_4DatabaseDataSet();
             this.nov17AssignmentsBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.project34DatabaseDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.nov17_AssignmentsTableAdapter = new Project_3_and_4._Project3_4DatabaseDataSetTableAdapters.Nov17_AssignmentsTableAdapter();
             this.chooseAssignmentLabel = new System.Windows.Forms.Label();
             this.skillsLabel = new System.Windows.Forms.Label();
@@ -43,10 +41,9 @@
             this.questionLabel = new System.Windows.Forms.Label();
             this.questionRichTextBox = new System.Windows.Forms.RichTextBox();
             this.openButton = new System.Windows.Forms.Button();
+            this.difficultyLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.nov17AssignmentsBindingSource1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this._Project3_4DatabaseDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nov17AssignmentsBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.project34DatabaseDataSetBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // listBox1
@@ -60,26 +57,6 @@
             this.listBox1.Size = new System.Drawing.Size(453, 604);
             this.listBox1.TabIndex = 1;
             this.listBox1.ValueMember = "Assignment Title";
-            // 
-            // nov17AssignmentsBindingSource1
-            // 
-            this.nov17AssignmentsBindingSource1.DataMember = "Nov17_Assignments";
-            this.nov17AssignmentsBindingSource1.DataSource = this._Project3_4DatabaseDataSet;
-            // 
-            // _Project3_4DatabaseDataSet
-            // 
-            this._Project3_4DatabaseDataSet.DataSetName = "_Project3_4DatabaseDataSet";
-            this._Project3_4DatabaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // nov17AssignmentsBindingSource
-            // 
-            this.nov17AssignmentsBindingSource.DataMember = "Nov17_Assignments";
-            this.nov17AssignmentsBindingSource.DataSource = this.project34DatabaseDataSetBindingSource;
-            // 
-            // project34DatabaseDataSetBindingSource
-            // 
-            this.project34DatabaseDataSetBindingSource.DataSource = this._Project3_4DatabaseDataSet;
-            this.project34DatabaseDataSetBindingSource.Position = 0;
             // 
             // nov17_AssignmentsTableAdapter
             // 
@@ -106,7 +83,7 @@
             // pointLabel
             // 
             this.pointLabel.AutoSize = true;
-            this.pointLabel.Location = new System.Drawing.Point(578, 145);
+            this.pointLabel.Location = new System.Drawing.Point(578, 119);
             this.pointLabel.Name = "pointLabel";
             this.pointLabel.Size = new System.Drawing.Size(181, 25);
             this.pointLabel.TabIndex = 4;
@@ -155,10 +132,20 @@
             this.openButton.Text = "Open";
             this.openButton.UseVisualStyleBackColor = true;
             // 
+            // difficultyLabel
+            // 
+            this.difficultyLabel.AutoSize = true;
+            this.difficultyLabel.Location = new System.Drawing.Point(578, 165);
+            this.difficultyLabel.Name = "difficultyLabel";
+            this.difficultyLabel.Size = new System.Drawing.Size(100, 25);
+            this.difficultyLabel.TabIndex = 10;
+            this.difficultyLabel.Text = "Difficulty:";
+            // 
             // StudentChooseAssignment
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.difficultyLabel);
             this.Controls.Add(this.openButton);
             this.Controls.Add(this.questionRichTextBox);
             this.Controls.Add(this.questionLabel);
@@ -171,9 +158,7 @@
             this.Name = "StudentChooseAssignment";
             this.Size = new System.Drawing.Size(1868, 731);
             ((System.ComponentModel.ISupportInitialize)(this.nov17AssignmentsBindingSource1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this._Project3_4DatabaseDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nov17AssignmentsBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.project34DatabaseDataSetBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -182,8 +167,6 @@
         #endregion
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.BindingSource nov17AssignmentsBindingSource;
-        private System.Windows.Forms.BindingSource project34DatabaseDataSetBindingSource;
-        private _Project3_4DatabaseDataSet _Project3_4DatabaseDataSet;
         private _Project3_4DatabaseDataSetTableAdapters.Nov17_AssignmentsTableAdapter nov17_AssignmentsTableAdapter;
         private System.Windows.Forms.BindingSource nov17AssignmentsBindingSource1;
         private System.Windows.Forms.Label chooseAssignmentLabel;
@@ -194,5 +177,6 @@
         private System.Windows.Forms.Label questionLabel;
         private System.Windows.Forms.RichTextBox questionRichTextBox;
         private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Label difficultyLabel;
     }
 }
