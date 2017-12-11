898ed176a2ad0024b24fa6b3cb5b0b50c9ebddc1
ISAACPC\isaactij <isaactij@utexas.edu>
2017-12-11 14:34:30 -0600

    Fixed last test

---
+++ Project 3 and 4/Teacher_Create_Assignment.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace Project_3_and_4
+{
+    partial class Teacher_Create_Assignment
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Teacher_Create_Assignment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "Teacher_Create_Assignment";
+            this.Load += new System.EventHandler(this.Teacher_Create_Assignment_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Project 3 and 4/Teacher_Create_Assignment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_3_and_4
+{
+    public partial class Teacher_Create_Assignment : UserControl
+    {
+        public Teacher_Create_Assignment()
+        {
+            InitializeComponent();
+        }
+
+        private void Teacher_Create_Assignment_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
